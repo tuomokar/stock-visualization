ce736ecb04e76b0a644f70747424abbcc79a0956
Tuomo Oila <tuomo.oila@gmail.com>
2020-09-19 17:14:09 +0300

    remove create-react-app unnecessary stuff

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,8 +2,9 @@ import React from 'react'
 import { render } from '@testing-library/react'
 import App from './App'
 
-test('renders learn react link', () => {
+test('renders Stockprices title', () => {
   const { getByText } = render(<App />)
-  const linkElement = getByText(/learn react/i)
-  expect(linkElement).toBeInTheDocument()
+
+  const title = getByText('Apple stockprices:')
+  expect(title).toBeInTheDocument()
 })
