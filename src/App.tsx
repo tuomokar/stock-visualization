b3f0f9375c9c90883fd40de36e7740df0bbdd90d
Tuomo Oila <tuomo.oila@gmail.com>
2020-09-20 16:46:15 +0300

    quick mockup with highcharts with the apple stocks data

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,41 @@
 import React from 'react'
+import Highcharts, { Options } from 'highcharts'
+import HighchartsReact from 'highcharts-react-official'
+
+import data from './webapi/mock.json'
 import './App.css'
+
+const timeSeriesObjects = data['Time Series (Daily)']
+
+const openValues = Object.keys(timeSeriesObjects)
+  .map(key => timeSeriesObjects[key as keyof typeof timeSeriesObjects])
+  .map(timeSeriesObject => Number(timeSeriesObject['1. open']))
+
+const testChartOptions: Options = {
+  title: {
+    text: ''
+  },
+  yAxis: {
+    title: {
+      text: 'Cost'
+    }
+  },
+  xAxis: {
+    title: {
+      text: 'Day'
+    }
+  },
+  // Typecast to any here as the types seem to require a type property in the object
+  // with 'area' value, but giving that, it paints the area and we don't really want
+  // that in a simple line chart
+  series: [{ data: openValues, name: '' } as any]
+}
 
 const App = () => {
   return (
     <div className='App'>
       <h1>Apple stockprices:</h1>
-      <div>Graph about the stockprices will be here</div>
+      <HighchartsReact highcharts={Highcharts} options={testChartOptions} />
     </div>
   )
 }
